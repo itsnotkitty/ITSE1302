d65785ead27c54002abe7993270161acaed0fdd8
Eric Gorski <erictgorski@gmail.com>
2017-09-12 15:36:58 -0500

    added constants for first and last name

--- Week_2/Week_2.groovy
+++ Week_2/Week_2.groovy
@@ -1,3 +1,11 @@
 // added printline Hello world
-println ("Hello World!")
+println "Hello World!"
   
+// Creating name constants
+class constant {
+       static final String FIRST_NAME = "Eric"
+       static final String LAST_NAME = "Gorski"
+}
+
+println (constant.FIRST_NAME)
+println (constant.LAST_NAME)
