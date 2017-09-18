928b431cf25fb7e595c70ea0b5cfdc42aec49604
Eric Gorski <erictgorski@gmail.com>
2017-09-18 15:29:54 -0500

    5 math problems using previosuly added variables

--- Week_2/Week_2.groovy
+++ Week_2/Week_2.groovy
@@ -12,5 +12,16 @@ println (constant.LAST_NAME)
 
 // testing of a leet number value.... who does that typing leet not 1337
 int leet = 1337; 
-assert b = leet / 42
+// defining b
+BigDecimal b = leet / 42
 println (b)
+
+//new Variable defined by Maths
+double bleet = 31.8333333333
+
+//5 math problems using defined variables
+println 4 * leet / 2 
+println 8 * bleet / 2  
+println bleet / leet
+println ((bleet *leet)/ 2 - 42 + 808) 
+println 1336 + 7331 / leet -1336
