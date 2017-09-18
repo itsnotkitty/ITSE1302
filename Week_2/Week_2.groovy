379de9c7fd051050157812c21cb912c4f84f75b6
Eric Gorski <erictgorski@gmail.com>
2017-09-18 15:52:41 -0500

    added comments answering questions for commit 6

--- Week_2/Week_2.groovy
+++ Week_2/Week_2.groovy
@@ -1,7 +1,10 @@
 // added printline Hello world
 println "Hello World!"
   
-// Creating name constants
+/* Creating name constants
+a Constant is a Value that never changes
+these can be numeric constants which is an unamed number whose value is unchanging
+or an string constant which is unchanign text */ 
 class constant {
        static final String FIRST_NAME = "Eric"
        static final String LAST_NAME = "Gorski"
@@ -10,16 +13,24 @@ class constant {
 println (constant.FIRST_NAME)
 println (constant.LAST_NAME)
 
-// testing of a leet number value.... who does that typing leet not 1337
+/* testing of a leet number value.... who does that typing leet not 1337 
+variables are named memory location whose content can vary over time */
 int leet = 1337; 
 println (leet)
 // defining b
 BigDecimal b = leet / 42
 println (b) 
+
 // to clarify b from non assigned numbers
-println "seperation of variables from math"
-//new Variable defined by Maths
+
+println "seperation of variables from maths"
+/* new Variable defined by Maths 
+Data types are defined as 
+"a particular kind of data item, as defined by the values it can take, the programming language used, or the operations that can be performed on it." 
+examples include String, Int, Double, BigDecimal, etc...*/
 double bleet = 31.8333333333
+
+//The difference between variable and constants is simply, Variable can change, Constants can not 
 
 //5 math problems using defined variables
 println 4 * leet / 2 
