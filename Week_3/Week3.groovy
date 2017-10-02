26d2395ae17ead633b5bea22202767c873c37351
Eric Gorski <erictgorski@gmail.com>
2017-10-02 14:35:39 -0500

    added full student list, averaged each students grade, and assigned letter grade values

--- Week_3/Week3.groovy
+++ Week_3/Week3.groovy
@@ -1,12 +1,90 @@
-Map<String,List<Integer>> studentGrades =[
+// make one for each student?
+
+Map<String,List<Integer>> jimGrades =[
     'Jim':[76,92,60,85,49,99]
-    'JMI':[70,82,40,75,99,10]
-    'MJI':[58,85,87,76,57,96]
-    'IJM':[10,11,12,13,14,100]
-    'Mij':[99,99,99,99,99,0]
 ]
 
-studentGrades.each { k,v ->
+jimGrades.each { k,v ->
+    BigDecimal average = v.sum() / v.size()
+    if (average >= 90 && average <= 100) {
+        letterGrade = 'A'
+    } else if (average < 60){
+        letterGrade = 'F'
+    } else if (average >= 80 && average <= 89.99) {
+        letterGrade = 'B'
+    } else if (average >= 70 && average <= 79.99) {
+        letterGrade = 'C'
+    } else if (averager >= 60 && average <= 69.99){
+        letterGrade = 'D'
+    }       
+        println "$k - $average - $letterGrade"
+}
+
+Map<String,List<Integer>> bobGrades =[
+    'Bob':[89,92,0,90,100,73]
+]
+
+bobGrades.each { k,v ->
+    BigDecimal average = v.sum() / v.size()
+    if (average >= 90 && average <= 100) {
+        letterGrade = 'A'
+    } else if (average < 60){
+        letterGrade = 'F'
+    } else if (average >= 80 && average <= 89.99) {
+        letterGrade = 'B'
+    } else if (average >= 70 && average <= 79.99) {
+        letterGrade = 'C'
+    } else if (averager >= 60 && average <= 69.99){
+        letterGrade = 'D'
+    }       
+        println "$k - $average - $letterGrade"
+}
+
+Map<String,List<Integer>> robouteGrades =[
+    'Roboute':[78,62,70,55,79,80]
+]
+
+robouteGrades.each { k,v ->
+    BigDecimal average = v.sum() / v.size()
+    if (average >= 90 && average <= 100) {
+        letterGrade = 'A'
+    } else if (average < 60){
+        letterGrade = 'F'
+    } else if (average >= 80 && average <= 89.99) {
+        letterGrade = 'B'
+    } else if (average >= 70 && average <= 79.99) {
+        letterGrade = 'C'
+    } else if (averager >= 60 && average <= 69.99){
+        letterGrade = 'D'
+    }       
+        println "$k - $average - $letterGrade"
+}
+
+Map<String,List<Integer>> mortyGrades =[
+    'Morty':[89,90,87,76,41,100]
+]
+
+mortyGrades.each { k,v ->
+    BigDecimal average = v.sum() / v.size()
+    if (average >= 90 && average <= 100) {
+        letterGrade = 'A'
+    } else if (average < 60){
+        letterGrade = 'F'
+    } else if (average >= 80 && average <= 89.99) {
+        letterGrade = 'B'
+    } else if (average >= 70 && average <= 79.99) {
+        letterGrade = 'C'
+    } else if (averager >= 60 && average <= 69.99){
+        letterGrade = 'D'
+    }       
+        println "$k - $average - $letterGrade"
+}
+
+Map<String,List<Integer>> destroGrades =[
+    'Destro the scourge of life':[16,22,4,15,39,32]
+]
+
+destroGrades.each { k,v ->
     BigDecimal average = v.sum() / v.size()
     if (average >= 90 && average <= 100) {
         letterGrade = 'A'
