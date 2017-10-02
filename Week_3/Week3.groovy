bcebd69d48ca8efc317e10cdceac6ce2dfb47a97
Eric Gorski <erictgorski@gmail.com>
2017-10-02 10:02:39 -0500

    initial commit

---
+++ Week_3/Week3.groovy
@@ -0,0 +1,11 @@
+Map<String,List<Integer>> studentGrades =[
+    'Jim':[76,92,60,85,49,99]
+    
+]
+
+studentGrade.each { k,v ->
+    BigDecimal average = v.sum() / v.size()
+    if (average >= 90 && <= 100 {
+        letterGrade = 'a'
+    } else if (average < 60
+}
