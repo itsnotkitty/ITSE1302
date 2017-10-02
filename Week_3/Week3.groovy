156d370604a23a835eef6210cee2dbda05756b30
Eric Gorski <erictgorski@gmail.com>
2017-10-02 10:24:14 -0500

    first iteration, needs work

--- Week_3/Week3.groovy
+++ Week_3/Week3.groovy
@@ -1,11 +1,23 @@
 Map<String,List<Integer>> studentGrades =[
     'Jim':[76,92,60,85,49,99]
-    
+    'JMI':[70,82,40,75,99,10]
+    'MJI':[58,85,87,76,57,96]
+    'IJM':[10,11,12,13,14,100]
+    'Mij':[99,99,99,99,99,0]
 ]
 
-studentGrade.each { k,v ->
+studentGrades.each { k,v ->
     BigDecimal average = v.sum() / v.size()
-    if (average >= 90 && <= 100 {
-        letterGrade = 'a'
-    } else if (average < 60
+    if (average >= 90 && average <= 100) {
+        letterGrade = 'A'
+    } else if (average < 60){
+        letterGrade = 'F'
+    } else if (average >= 80 && average <= 89.99) {
+        letterGrade = 'B'
+    } else if (average >= 70 && average <= 79.99) {
+        letterGrade = 'C'
+    } else if (averager >= 60 && average <= 69.99){
+        letterGrade = 'D'
+    }       
+        println "$k - $average - $letterGrade"
 }
