682e3100bfc5861337c11f20a5481c87c599621b
Eric Gorski <erictgorski@gmail.com>
2017-10-02 20:46:25 -0500

    modified a comment

--- Week_3/Week3.groovy
+++ Week_3/Week3.groovy
@@ -4,9 +4,8 @@ Map<String,List<Integer>> jimGrades =[
     'Jim':[76,92,60,85,49,99]
 ]
 
-/** loop designed to average strudents grades. Had trouble with else if (control structure) statment when testing. Realized that the average was 
+/** loop designed to average students grades. Had trouble with else if (control structure) statment when testing. Realized that the average was 
 a value I had left off of course there would be an issue when the resulting average had no value in which to be assigned
-honestly not sure if there is a loop here, I have a control structure, but what makes it a loop if this is one? will be submitted as is so it's not late
 **/
 jimGrades.each { k,v ->
     BigDecimal average = v.sum() / v.size()
